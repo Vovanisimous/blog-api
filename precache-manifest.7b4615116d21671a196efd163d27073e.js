9bbf60e20b1ad5b06abcf7a70f8a01ee2c38c910
Vovanisimous <vovanisimous@yandex.com>
2020-08-28 13:24:13 +0300

    Updates

---
+++ precache-manifest.7b4615116d21671a196efd163d27073e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edd6d84327559fa95dc4c5e6f760395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6898db78a5586714439",
+    "url": "/static/css/main.4e340104.chunk.css"
+  },
+  {
+    "revision": "6cbf0e888cee3de9058e",
+    "url": "/static/js/2.cdfd2570.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.cdfd2570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6898db78a5586714439",
+    "url": "/static/js/main.2c24f5b5.chunk.js"
+  },
+  {
+    "revision": "3f432f31e97d426be848",
+    "url": "/static/js/runtime-main.82310c28.js"
+  }
+]);
--- publish.js
+++
@@ -1,6 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish("build", {
-    branch: "deploy",
-    repo: "https://github.com/Vovanisimous/Blog-api.git"
-}, (msg) => console.log(msg));
